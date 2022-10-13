d28ad0979094353b883c24e876d9efc42dbc7602
Gyanu15R <gyanendrakumar.infoicon@gmail.com>
2022-10-13 17:46:06 +0530

    Initial commit

---
+++ src/Dfgh.java
@@ -0,0 +1,15 @@
+public class Dfgh {
+    public static void main (String[] args)
+    {
+      int a = 10;
+      int b = 20;
+      Boolean c = (a>b) ;
+      System.out.println(c);
+
+
+    }
+
+
+
+
+}
