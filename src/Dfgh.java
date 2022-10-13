5fe9ab288f7a49ceb16744bebfbd303935da478c
Gyanu15R <gyanendrakumar.infoicon@gmail.com>
2022-10-13 17:56:43 +0530

    Update Program

--- src/Dfgh.java
+++ src/Dfgh.java
@@ -3,9 +3,14 @@ public class Dfgh {
     {
       int a = 10;
       int b = 20;
-      Boolean c = (a>b) ;
-      System.out.println(c);
+      Boolean c;
 
+     if( c = (a>b))
+     {
+         System.out.println( "THe condition is :"+c);
+     }
+     else
+        System.out.println("THe condition is :"+c);
 
     }
 
